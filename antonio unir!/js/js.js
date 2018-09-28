2fa1ce87ab83d126d2d2b842a249eb8bad96872b
Roberto912 <41331649+Roberto912@users.noreply.github.com>
2018-09-28 14:26:57 +0100

    antonio navbar,desplegable Falta estilo y mas bootstrap

--- antonio unir!/js/js.js
+++ antonio unir!/js/js.js
@@ -1,12 +1,22 @@
+
 //Mueve el menu para la dereche y quita el buton de esta funcion y pone el otro 
-function play(){
-    document.getElementById("play").style.marginLeft = "0";
-    document.getElementById("linerplay").style.display= "none";
-    document.getElementById("lineroff").style.display= "block";
-    };
-    //Mueve el menu a su inicio y quita el buton de esta funcion y pone el otro 
-      function off(){
-        document.getElementById("play").style.marginLeft = "-85%"
-        document.getElementById("linerplay").style.display= "block"
-        document.getElementById("lineroff").style.display= "none"
-      };
+function Desplegar(){
+document.getElementById("play").style.marginLeft = "0";
+document.getElementById("linerplay").style.display= "none";
+document.getElementById("lineroff").style.display= "block";
+};
+//Mueve el menu a su inicio y quita el buton de esta funcion y pone el otro 
+  function Quitar(){
+    document.getElementById("play").style.marginLeft = "-85%"
+    document.getElementById("linerplay").style.display= "block"
+    document.getElementById("lineroff").style.display= "none"
+
+
+
+};
+
+
+
+
+
+
