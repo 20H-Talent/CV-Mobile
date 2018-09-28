2d48eb4db4d997abac2fdf1159cebb94e8697b9c
Roberto912 <41331649+Roberto912@users.noreply.github.com>
2018-09-28 11:57:45 +0100

    layout

---
+++ antonio unir!/js/js.js
@@ -0,0 +1,12 @@
+//Mueve el menu para la dereche y quita el buton de esta funcion y pone el otro 
+function play(){
+    document.getElementById("play").style.marginLeft = "0";
+    document.getElementById("linerplay").style.display= "none";
+    document.getElementById("lineroff").style.display= "block";
+    };
+    //Mueve el menu a su inicio y quita el buton de esta funcion y pone el otro 
+      function off(){
+        document.getElementById("play").style.marginLeft = "-85%"
+        document.getElementById("linerplay").style.display= "block"
+        document.getElementById("lineroff").style.display= "none"
+      };
