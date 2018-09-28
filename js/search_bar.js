66ba10cf8ac92013aaa7a4b2cf0c894c84c92ab0
María Naranjo Almeida <maria2000naranjo@gmail.com>
2018-09-28 12:04:44 +0100

    Updated search_bar.js

--- js/search_bar.js
+++ js/search_bar.js
@@ -1,7 +1,27 @@
-var a = document.getElementById('search');
-a.addEventListener('submit',function(e) {
-    e.preventDefault();
-    var b = document.getElementById('search_input').value;
-    window.location.href = '../index.html'+b;
+// document.getElementById("btn").addEventListener("click", function() {
+//     $.ajax({
+//       url: "https://jsonplaceholder.typicode.com/users"
+//     }).done(function(data) {
 
-});
+// });
+// });
+var a = document.getElementById('search');
+var b = document.getElementById('search_input').value;
+var myElement = document.getElementById("cards_container");
+//     }
+
+// function searchUsers (data, input) {
+//     if (b = myElement) {
+
+//         console.log("Found user");
+//     }
+// }
+
+// function myFunction() {
+    // var x = document.forms["search"];
+    // var text = ""
+    // var i;
+    // for (i = 0; i < x.length ;i++) {
+    //     text += x.elements[i].value + "<br>";
+    // }
+}
