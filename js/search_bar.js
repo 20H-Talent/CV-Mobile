04a0f4c6f65fece7de23ebb46595fff1f0800ea1
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-10-03 12:36:56 +0100

    Filtering search results separated by data type

--- js/search_bar.js
+++ js/search_bar.js
@@ -3,7 +3,24 @@ searchInput.addEventListener("keyup", function(e) {
   const searchTerm = searchInput.value;
   const users = document.querySelectorAll('.card');
 
+  const priorityTable = ['name', 'username', 'email', 'company'];
+
+  // User data separated for type
+  const namesArray = [];
+  const usernamesArray = [];
+  const emailsArray = [];
+  const companiesArray = [];
+
+  function filterByData(data, reference, store) {
+    if (data.toLowerCase() !== reference.slice(0, data.length).toLowerCase()) {
+      store.push(null);
+    } else {
+      store.push(reference);
+    }
+  }
+
   if (searchTerm) {
+    // Filter the users and store the results on the arrays above
     users.forEach( user => {
       // Save every user data in a variable for future comparison
       const userName = user.childNodes[1].childNodes[0].childNodes[0].textContent;
@@ -11,13 +28,27 @@ searchInput.addEventListener("keyup", function(e) {
       const userEmail = user.childNodes[1].childNodes[2].childNodes[1].textContent;
       const userCompany = user.childNodes[1].childNodes[3].childNodes[1].textContent;
 
-      // comparar el searchTerm con la misma cantidad de caracteres del nombre del usuario
-      if (searchTerm.toLowerCase() !== userName.slice(0, searchTerm.length).toLowerCase()) {
-        user.classList.add('d-none');
-      } else {
-        user.classList.remove('d-none');
-      }
+      // Filter by name
+      filterByData(searchTerm, userName, namesArray);
+
+      // Filter by username
+      filterByData(searchTerm, userUsername, usernamesArray);
+
+      // Filter by email
+      filterByData(searchTerm, userEmail, emailsArray);
+
+      // Filter by company
+      filterByData(searchTerm, userCompany, companiesArray);
+
     });
+
+    // Once the data is separated its iterated by priority to show the correct users
+    // Check if there is at least one value that is not null inside the array
+    console.log('names: ', namesArray);
+    console.log('usernames: ', usernamesArray);
+    console.log('emails: ', emailsArray);
+    console.log('companies: ', companiesArray);
+
   } else {
     users.forEach( user => user.classList.remove('d-none'));
   }
