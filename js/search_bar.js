297a315ec409747ed4e1966cbdec63db1490de16
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-10-03 11:16:10 +0100

    saving the user info in diferent variables for future filtering by data priority

--- js/search_bar.js
+++ js/search_bar.js
@@ -5,7 +5,11 @@ searchInput.addEventListener("keyup", function(e) {
 
   if (searchTerm) {
     users.forEach( user => {
+      // Save every user data in a variable for future comparison
       const userName = user.childNodes[1].childNodes[0].childNodes[0].textContent;
+      const userUsername = user.childNodes[1].childNodes[1].childNodes[1].textContent;
+      const userEmail = user.childNodes[1].childNodes[2].childNodes[1].textContent;
+      const userCompany = user.childNodes[1].childNodes[3].childNodes[1].textContent;
 
       // comparar el searchTerm con la misma cantidad de caracteres del nombre del usuario
       if (searchTerm.toLowerCase() !== userName.slice(0, searchTerm.length).toLowerCase()) {
