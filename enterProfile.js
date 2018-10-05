44e60cd9191d60453e72dce11327514b5133ad27
Tony <rodriguezmellado.antonio@gmail.com>
2018-10-05 11:44:40 +0100

    add enter profile

---
+++ enterProfile.js
@@ -0,0 +1,14 @@
+$.ajax({
+    method: "POST",
+    url: "https://jsonplaceholder.typicode.com/users"
+    data: JSON.stringify({ name: "Leanne Graham" }),
+    contentType: "application/json"
+  });
+
+  var cards_profile = (
+    '<h5 class="card-title"><b>' + Leanne Graham + '</b></h5>' +
+    '<p class="card-text">Username: <b>' + Bret + '</b></p>' +
+    '<p class="card-text">Email: <b>' + Sincere@april.biz + '</b></p>' +
+    '<p class="card-text">city: <b>' + Gwenborough + '</b></p>' +
+
+  );
