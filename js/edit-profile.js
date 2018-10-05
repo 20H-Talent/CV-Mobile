4b277e9f2ab2c666ed9168c13309b9c6cbae42ae
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-10-05 19:04:42 +0100

    refactored edit-profile.js code

--- js/edit-profile.js
+++ js/edit-profile.js
@@ -1,14 +1,14 @@
 function edit() {
     var Button = document.getElementById("edit");
-    if (Button.className == "w-25 border border-info mt-2 rounded") {
+    if (!Button.className.includes('editing')) {
         Button.innerHTML = "Save";
-        Button.className += " editing";
+        Button.classList.add('editing');
         document.querySelectorAll('p').forEach(el => el.setAttribute('contenteditable', true));
         document.querySelector('h2').setAttribute('contenteditable', true);
     } else {
         document.querySelectorAll('p').forEach(el => { el.setAttribute('contenteditable', false); console.log(el.textContent); });
         document.querySelector('h2').setAttribute('contenteditable', false);
-        Button.className = "w-25 border border-info mt-2 rounded";
+        Button.classList.remove('editing');
         Button.innerHTML = "Edit";
     }
 };
