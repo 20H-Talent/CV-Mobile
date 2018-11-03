cac580ece37d3931e872a3e2ce14fa0343845bc2
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-11-03 19:25:03 +0000

    removed unnecesarry files from the project in order to simplify

--- js/surveys.js
+++ js/surveys.js
@@ -1,5 +1,68 @@
+// node-fetch required to be able to live test the code with quokka.js
+const fetch = require('node-fetch');
+
 // Render a form inside an accordion dynamically from the surveys stored into the api
 // 1 - Connect to the api and get the surveys
-// 2 - Order the surveys by its starting date
-// 3 -
-// NEW SURVEYS /
+// 2 - Render an accordion that will hold the forms
+// 2 - Render a form component for each survey inside the accordion
+// 3 - Render the correct type of input for each form element
+
+
+const surveysURL = 'https://cv-mobile-api.herokuapp.com/api/surveys';
+const container = document.querySelector('.main-container');
+
+class SurveysPage {
+  constructor(url) {
+    this._url = url;
+    this._surveys;
+  }
+
+  get surveys() {
+    return this._surveys;
+  }
+
+  get numOfSurveys() {
+    return this._surveys.length;
+  }
+
+  getDataFromApi(url) {
+    fetch(url)
+      .then( res => res.json())
+      .then( data => {
+        this._surveys = data;
+        this.renderAccordion(this._surveys);
+      })
+      .catch( err => console.log(err.message));
+  }
+
+  renderAccordion(surveysArr) {
+    // console.log(surveysArr);
+  }
+
+  init() {
+    this.getDataFromApi(this._url);
+  }
+}
+
+class SurveyForm {
+  constructor(header, elements) {
+    this._header = header,
+    this._header = elements
+  }
+
+  renderHeader(header) {}
+  renderInput(element) {}
+  renderForm() {}
+}
+
+
+
+// Initialization of the page's functionality
+let pageHandler = new SurveysPage(surveysURL);
+window.onload = pageHandler.init();
+
+// Export functions for unit testing
+module.exports = {
+  SurveysPage: SurveysPage,
+  SurveyForm: SurveyForm
+}
--- tests/surveys.test.js
+++
@@ -1,5 +0,0 @@
-// UNIT TESTING FOR THE SURVEYS PAGE
-
-describe('Surveys.js test suite', () => {
-  it('should run the test', () => {});
-});
