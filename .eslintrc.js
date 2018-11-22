1a0e71d3ba36130c29f495ce65225203c7582ddc
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-11-22 04:22:33 +0000

    inialized eslint to cleanup new and existing code

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb",
+    "globals": {
+        "window": true,
+        "document": true,
+        "$": true
+    },
+    "env": {
+        "browser": true,
+        "node": true
+    }
+};
